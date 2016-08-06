62ef4e582b3800f0114965fb36f6f952adc45351
Ed Herbst <ed.herbst@gmail.com>
2016-08-06 09:03:05 -0400

    fix conda

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,6 @@
 make clean
-make tpf_driver_nkmp
-cp tpf_driver_nkmp $PREFIX/bin
+make tpf_driver
+cp tpf_driver $PREFIX/bin
 mkdir -p $PREFIX/include/tempered_pf
 cp src/us.txt $PREFIX/include/tempered_pf
 cp p0.txt $PREFIX/include/tempered_pf
